afaac66d9fefb9ff89545e0eaa7b9beae60a354a
moinul75 <102654562+moinul75@users.noreply.github.com>
2022-04-04 15:17:52 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+#if else using
+
+num1 = 10
+num2 = 20
+
+if num1> num2:
+    print("The largest number is: ", num1)
+else:
+    print("The largest Number is: ", num2)
+    
+# ternary Operator 
+
+print("The largest number is: ", num1 if num1> num2 else num2)
